4b1e91867c93f13d12972eb9cb5eb093a00e6084
Aran Murphy <aranwmurphy@gmail.com>
2024-05-12 23:00:32 -0600

    Added tests to infra_test.go

--- infra/infra_test.go
+++ infra/infra_test.go
@@ -3,13 +3,13 @@ package main
 // import (
 // 	"testing"
 
-// 	"github.com/aws/aws-cdk-openapi/awscdk/v2"
-// 	"github.com/aws/aws-cdk-openapi/awscdk/v2/assertions"
+// 	"github.com/aws/aws-cdk-go/awscdk/v2"
+// 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
 // 	"github.com/aws/jsii-runtime-go"
 // )
 
 // example tests. To run these tests, uncomment this file along with the
-// example resource in infra_test.openapi
+// example resource in infra_test.go
 // func TestInfraStack(t *testing.T) {
 // 	// GIVEN
 // 	app := awscdk.NewApp(nil)
