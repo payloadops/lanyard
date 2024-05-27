518502f22dd75cc67871a4e9428c7fa752747843
Aran Murphy <aranwmurphy@gmail.com>
2024-05-26 21:12:46 -0600

    Added codegen script for the openapi routes

--- spec/codegen.sh
+++ spec/codegen.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# Generate the server side stubs for the openapi spec, excluding the service modules (very important)
 openapi-generator generate -g go-server -i ./spec/spec.v1.yaml -o ../api --additional-properties=router=chi,packageName=openapi,outputAsLibrary=true,sourceFolder=openapi,onlyInterfaces=true
