8232746c3960c8df9901df68305ed33eb0f26147
Aran Murphy <aranwmurphy@gmail.com>
2024-05-27 00:34:03 -0600

    Added alias inside bash script

--- spec/codegen.sh
+++ spec/codegen.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+if [ -e "./openapi-generator-cli.jar" ]; then
+  alias openapi-generator="java -jar ./openapi-generator-cli.jar"
+fi
+
 # Generate the server side stubs for the openapi spec, excluding the service modules (very important)
 openapi-generator generate -g go-server -i ./spec/spec.v1.yaml -o ../app --additional-properties=router=chi,packageName=openapi,outputAsLibrary=true,sourceFolder=openapi,onlyInterfaces=true
