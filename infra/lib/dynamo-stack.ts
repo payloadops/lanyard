3c54e1b5ba5deb1a26bb1cb55e4dc20662133c79
Ryan Krumbholz <krumbholz98@gmail.com>
2024-05-30 08:06:54 -0700

    add prompts bucket and keys table

--- infra/lib/dynamo-stack.ts
+++ infra/lib/dynamo-stack.ts
@@ -9,39 +9,53 @@ export class DynamoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, stage: string, props?: cdk.StackProps) {
     super(scope, id, props);
     new dynamodb.Table(this, 'ProjectsTable', {
-        tableName: "projects",
+        tableName: "Projects",
         partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING},
         sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING},
         replicationRegions: stage === Stages.PROD ? REPLICATIONS_REGIONS : undefined,
         billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-        tableClass: dynamodb.TableClass.STANDARD
+        tableClass: dynamodb.TableClass.STANDARD,
+        // removalPolicy: cdk.RemovalPolicy.RETAIN
       })
 
     new dynamodb.Table(this, 'PromptsTable', {
-        tableName: "prompts",
+        tableName: "Prompts",
         partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING},
         sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING},
         replicationRegions: REPLICATIONS_REGIONS,
         billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-        tableClass: dynamodb.TableClass.STANDARD
+        tableClass: dynamodb.TableClass.STANDARD,
+        // removalPolicy: cdk.RemovalPolicy.RETAIN
     })
 
     new dynamodb.Table(this, 'BranchesTable', {
-        tableName: "branches",
+        tableName: "Branches",
         partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING},
         sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING},
         replicationRegions: REPLICATIONS_REGIONS,
         billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-        tableClass: dynamodb.TableClass.STANDARD
+        tableClass: dynamodb.TableClass.STANDARD,
+        // removalPolicy: cdk.RemovalPolicy.RETAIN
       })
 
     new dynamodb.Table(this, 'CommitsTable', {
-        tableName: "commits",
+        tableName: "Commits",
         partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING},
         sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING},
         replicationRegions: REPLICATIONS_REGIONS,
         billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-        tableClass: dynamodb.TableClass.STANDARD
+        tableClass: dynamodb.TableClass.STANDARD,
+        // removalPolicy: cdk.RemovalPolicy.RETAIN
     })
+
+    new dynamodb.Table(this, 'APIKeysTable', {
+      tableName: "APIKeys",
+      partitionKey: { name: 'pk', type: dynamodb.AttributeType.STRING},
+      sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING},
+      replicationRegions: REPLICATIONS_REGIONS,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      tableClass: dynamodb.TableClass.STANDARD,
+      // removalPolicy: cdk.RemovalPolicy.RETAIN
+  })
   }
 }
