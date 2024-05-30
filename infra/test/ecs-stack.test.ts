5714931eff40e04716a390e18f5b960b29fe2efa
Ryan Krumbholz <krumbholz98@gmail.com>
2024-05-30 12:11:06 -0700

    cdk test

--- infra/test/ecs-stack.test.ts
+++ infra/test/ecs-stack.test.ts
@@ -9,7 +9,7 @@ import Stages from '../lib/constants/stages';
 test('ECS Cluster Created', () => {
   const app = new cdk.App();
   const vpcStack = new VpcStack(app, 'TestVpcStack', Stages.DEV);
-  const stack = new EcsStack(app, 'TestEcsStack', vpcStack, Stages.DEV);
+  const stack = new EcsStack(app, 'TestEcsStack', vpcStack, "bucket-name", Stages.DEV);
   const template = Template.fromStack(stack);
   
   template.hasResource('AWS::ECS::Cluster', {});
