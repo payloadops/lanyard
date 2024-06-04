740b43f7aaf33969ba7688949829844436cf52d8
Ryan Krumbholz <krumbholz98@gmail.com>
2024-06-03 18:03:40 -0700

    update vpc test

--- infra/test/vpc-stack.test.ts
+++ infra/test/vpc-stack.test.ts
@@ -6,7 +6,9 @@ import Stages from '../lib/constants/stages';
 
 test('VPC Created with Correct CIDR', () => {
   const app = new cdk.App();
-  const stack = new VpcStack(app, 'TestVpcStack', Stages.DEV);
+  const stack = new VpcStack(app, 'TestVpcStack', {
+    stage: Stages.DEV
+  });
   const template = Template.fromStack(stack);
   
   template.hasResourceProperties('AWS::EC2::VPC', {
